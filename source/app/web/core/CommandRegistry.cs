746da014d5dd36cb02bb4c1fa8dcdbf084c24fc7
anortham <anortham@gmail.com>
2012-01-25 12:36:08 -0600

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -1,10 +1,26 @@
-﻿namespace app.web.core
+﻿using System;
+using System.Collections.Generic;
+
+namespace app.web.core
 {
   public class CommandRegistry : IFindCommands
   {
+      IEnumerable<IProcessOneRequest> commands;
+ 
+      public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
+      {
+          this.commands = commands;
+      }
+
     public IProcessOneRequest get_the_command_that_can_process(IProvideDetailsToCommands request)
     {
-      throw new System.NotImplementedException();
+        foreach ( var command in commands )
+        {
+            if (command.can_process(request))
+                return command;
+        }
+
+        throw new ArgumentException("I don't have a command that can process that request.");
     }
   }
 }
