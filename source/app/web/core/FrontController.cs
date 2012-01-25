75d49a5e1ee714a01dcb74462cb9aa6e04e8bc76
anortham <anortham@gmail.com>
2012-01-25 12:19:05 -0600

    Pushing new changes

--- source/app/web/core/FrontController.cs
+++ source/app/web/core/FrontController.cs
@@ -2,9 +2,17 @@
 {
   public class FrontController : IProcessRequests
   {
+      IFindCommands commandFinder;
+
+      public FrontController(IFindCommands commandFinder)
+      {
+          this.commandFinder = commandFinder;
+      }
+
     public void process(IProvideDetailsToCommands request)
     {
-      throw new System.NotImplementedException();
+        var result = commandFinder.get_the_command_that_can_process( request );
+        result.process(request);
     }
   }
 }
