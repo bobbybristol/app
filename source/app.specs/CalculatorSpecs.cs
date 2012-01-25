0bda6abb0065e96ccf4d6735c322641246002809
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-01-24 18:40:15 -0700

    Updated

---
+++ source/app.specs/CalculatorSpecs.cs
@@ -0,0 +1,24 @@
+﻿using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+  [Subject(typeof(Calculator))]
+  public class CalculatorSpecs
+  {
+    public abstract class concern : Observes<Calculator>
+    {
+    }
+
+    public class when_adding_two_numbers : concern
+    {
+      Because b = () =>
+        result = Calculator.add(2, 3);
+
+      It should_return_the_sum = () =>
+        result.ShouldEqual(5);
+
+      static int result;
+    }
+  }
+}
