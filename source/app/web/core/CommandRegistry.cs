0a46d50dad2215adaf031d084f46f658a0409faf
anortham <anortham@gmail.com>
2012-01-25 12:50:16 -0600

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace app.web.core
@@ -6,15 +7,19 @@ namespace app.web.core
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IProcessOneRequest> commands;
+      IProcessOneRequest unknownRequestProcessor;
 
-    public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
+    public CommandRegistry(IEnumerable<IProcessOneRequest> commands, IProcessOneRequest unknownRequestProcessor)
     {
       this.commands = commands;
+        this.unknownRequestProcessor = unknownRequestProcessor;
     }
 
     public IProcessOneRequest get_the_command_that_can_process(IProvideDetailsToCommands request)
     {
-      return commands.First(x => x.can_process(request));
+        var command = commands.FirstOrDefault( x => x.can_process( request ) );
+
+        return command ?? unknownRequestProcessor; 
     }
   }
 }
