a43b3aef5721e9b3b9257496d1bdd14a7964e0b9
Bobby Bristol <bobbybristol@gmail.com>
2012-01-26 09:30:04 -0500

    Pushing new changes

---
+++ source/app.console/DropBoxFileEvent/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DropBoxFileEvent
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Input how many file events");
+            int line = Console.Read();
+            int thisline = 0;
+            while (thisline < line)
+            {
+                
+            }
+        }
+    }
+}
--- source/app/FileEvent.cs
+++ source/app/FileEvent.cs
@@ -1,8 +1,25 @@
-﻿using F
+﻿
 namespace app
 {
     public class FileEvent
     {
-         
+        private string[] FileEvents;
+       public FileEvent(params string[] fileEvents )
+       {
+           this.FileEvents = fileEvents;
+       }
+        public void Process()
+        {
+            foreach(string fileEvent in FileEvents)
+            {
+                
+            }
+        }
+
+        public  void CheckEvent(string fileEvent)
+        {
+
+        }
+
     }
 }
